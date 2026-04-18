c70399485cb985b5d9ece4ca598efabd5d1181d9
rajdeep1111111111-dot <rajdeep1111111111@gmail.com>
2026-04-18 15:00:53 +0800

    Fix: src/proxy.ts, now does not run the proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,6 +7,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|auth|$).*)",
+    "/((?!_next/static|_next/image|_next/data|favicon\\.ico|favicon-16x16\\.png|favicon-32x32\\.png|apple-touch-icon\\.png|site\\.webmanifest|android-chrome-192x192\\.png|android-chrome-512x512\\.png|api/|auth).*)",
   ],
 };
